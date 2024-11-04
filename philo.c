a9d2b061409bdefddf2ea0c0c657d6845d40b105
mjuicha <juichamohamedamine5002@gmail.com>
2024-11-04 17:52:43 +0100

    fill struct

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: mjuicha <mjuicha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 17:02:38 by mjuicha           #+#    #+#             */
-/*   Updated: 2024/11/03 18:22:29 by mjuicha          ###   ########.fr       */
+/*   Updated: 2024/11/04 17:52:26 by mjuicha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,26 @@ int     check_input(int ac, char **av)
     return (1);
 }
 
-int     is_valid_input(int ac, char **av)
+void    philo_info(t_philo *philo, int ac, char **av)
+{
+    if (!av)
+        return ;
+    philo->philo_nb = ft_atoi(av[1]);
+    philo->t_die = ft_atoi(av[2]);
+    philo->t_eat = ft_atoi(av[3]);
+    philo->t_sleep = ft_atoi(av[4]);
+    if (ac == 6)
+        philo->nb_of_meals = ft_atoi(av[5]);
+    
+}
+int     check_philo(t_philo *philo)
+{
+    if (philo->philo_nb < 0 || philo->t_die < 0 || philo->t_eat < 0 || philo->t_sleep < 0 || philo->nb_of_meals < 0)
+        return (1);
+    return (0);
+}
+
+int     is_valid_input(int ac, char **av, t_philo *philo)
 {
     if (ac != 5 && ac != 6)
     {
@@ -58,13 +77,21 @@ int     is_valid_input(int ac, char **av)
         printf("Error: wrong arguments\n");
         return (0);
     }
+    philo_info(philo, ac, av);
+    if (check_philo(philo))
+        return (printf("Error: wrong arguments\n"), 0);
     printf("Everything is fine\n");
+    
     return (1);
 }
 
 int main(int ac, char **av)
 {
-    if (!is_valid_input(ac, av))
+    t_philo *philo = malloc(sizeof(t_philo));
+    if (!philo)
         return (1);
+    if (!is_valid_input(ac, av, philo))
+        return (1);
+    // printf("philo_nb: %d\nt_die: %d\nt_eat: %d\nt_sleep: %d\nnb_of_meals: %d\n", philo->philo_nb, philo->t_die, philo->t_eat, philo->t_sleep, philo->nb_of_meals);
     return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: mjuicha <mjuicha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 17:03:06 by mjuicha           #+#    #+#             */
-/*   Updated: 2024/11/03 17:05:01 by mjuicha          ###   ########.fr       */
+/*   Updated: 2024/11/04 17:50:21 by mjuicha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,14 @@
 
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
+
+int     ft_atoi(const char *str);
+typedef struct  s_philo
+{
+    int             philo_nb;
+    int             t_die;
+    int             t_eat;
+    int             t_sleep;
+    int             nb_of_meals;
+}               t_philo;
