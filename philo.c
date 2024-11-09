184383a639c33cf5cb47b483b4b3f4fc3b81021b
mjuicha <juichamohamedamine5002@gmail.com>
2024-11-09 20:48:42 +0100

    philo is working now %

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: mjuicha <mjuicha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 17:02:38 by mjuicha           #+#    #+#             */
-/*   Updated: 2024/11/08 19:29:00 by mjuicha          ###   ########.fr       */
+/*   Updated: 2024/11/09 20:46:30 by mjuicha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ void    philo_info(t_philo *philo, int ac, char **av)
 }
 int     check_philo(t_philo *philo)
 {
-    if (philo->philo_nb <= 0 || philo->t_die <= 0 || philo->t_eat <= 0 || philo->t_sleep <= 0 || philo->nb_of_meals <= 0)
+    if (philo->philo_nb <= 0 || philo->t_die <= 0 || philo->t_eat <= 0 || philo->t_sleep <= 0)
         return (1);
     return (0);
 }
@@ -69,21 +69,27 @@ int     is_valid_input(int ac, char **av, t_philo *philo)
 {
     if (ac != 5 && ac != 6)
     {
-        printf("\033[31;1mError: wrong numbers of arguments\033[0m\n");
+        printf("Error: wrong numbers of arguments\n");
         return (0);
     }
     if (check_input(ac, av) == 0)
     {
-        printf("\033[31;1mError: wrong arguments\033[0m\n");
+        printf("Error: wrong arguments\n");
         return (0);
     }
     philo_info(philo, ac, av);
     if (check_philo(philo))
-        return (printf("\033[31;1mError: wrong arguments\033[0m\n"), 0);
-    printf("\033[32;1mEverything is fine\033[0m\n");
-    
+        return (printf("Error: wrong arguments\n"), 0);
     return (1);
 }
+
+long    get_time(void)
+{
+    struct timeval  tv;
+    gettimeofday(&tv, NULL);
+    return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
 t_info  *create_philos(t_philo *philo)
 {
     t_info *info = malloc(sizeof(t_info));
@@ -93,6 +99,8 @@ t_info  *create_philos(t_philo *philo)
     info->threads = malloc(sizeof(pthread_t) * philo->philo_nb);
     if (!info->threads || !info->forks)
         return (NULL);
+    philo->is_time = 0;
+    philo->is_died = 0;
     int i = 0;
     pthread_t thread;
     while (i < philo->philo_nb)
@@ -104,22 +112,125 @@ t_info  *create_philos(t_philo *philo)
     return (info);
 }
 
-void    *philo_lanch(void *arg)
+void    philo_lanch(void *arg)
 {
     t_philo *philo = (t_philo *)arg;
     int i = 0;
     philo->info = create_philos(philo);
+    pthread_mutex_t n_mutex;
+    philo->mutex = n_mutex;
+    pthread_mutex_init(&philo->mutex, NULL);
     if (!philo->info)
-        printf("\x1b[31mPhilosophers Error\033[0m\n");
-    printf("\x1b[32mPhilosophers created\033[0m\n");
+        printf("Philosophers Error\n");
+}
+
+int    right_fork(t_phil *philos)
+{
+    return (philos->id);
+}
+
+int    left_fork(t_phil *philos)
+{
+    if (philos->id == 0)
+        return (philos->nb_philo - 1);
+    return (philos->id - 1);
+}
+void    philo_msg(t_phil *philos, char *msg)
+{
+    printf("%ld %d %s\n", get_time() - philos->philoss->start_time, philos->fake_id, msg);
+}
+void    take_fork(t_phil *philos)
+{
+    pthread_mutex_lock(&philos->info->forks[philos->right]);
+    pthread_mutex_lock(&philos->info->forks[philos->left]);
+    if (philos->philoss->is_time == 0)
+    {
+        philos->philoss->start_time = get_time();
+        philos->philoss->is_time = 1;
+    }
+    philo_msg(philos, "has taken a fork");
+    if (philos->philoss->philo_nb == 1)
+    {
+        usleep(philos->philoss->t_die * 1000);
+        philos->philoss->is_died = 1;
+    }
+    philo_msg(philos, "has taken a fork");
+    // printf("\x1b[34;1mLOCKED MUTEX ARE : %d and %d\033[0m\n", philos->right, philos->left);
+    // printf("Philosopher %d has taken a fork\n", philos->fake_id);
+    // printf("Philosopher %d has taken a fork\n", philos->fake_id);
+}
+void    down_fork(t_phil *philos)
+{
+    pthread_mutex_unlock(&philos->info->forks[philos->right]);
+    pthread_mutex_unlock(&philos->info->forks[philos->left]);
+    // printf("\x1b[31;1mUNLOCKED MUTEX ARE : %d and %d\033[0m\n", philos->right, philos->left);
+}
+void    eating(t_phil *philos)
+{
+    philo_msg(philos, "is eating");
+    philos->philoss->last_meal = get_time();
+    usleep(philos->philoss->t_eat * 1000);
+}
+
+void    sleeping(t_phil *philos)
+{
+    philo_msg(philos, "is sleeping");
+    usleep(philos->philoss->t_sleep * 1000);
+    philo_msg(philos, "is thinking");
+}
+void    *philoso(void *arg)
+{
+    t_phil *philos = (t_phil *)arg;
+    while (!philos->philoss->is_died)
+    {
+        t_phil *philos = (t_phil *)arg;
+        philos->right = right_fork(philos);
+        philos->left = left_fork(philos);
+        take_fork(philos);
+        eating(philos);
+        down_fork(philos);
+        sleeping(philos);
+    }
+    return (NULL);
+}
+
+void    *check_death(void *arg)
+{
+    t_phil *philos = (t_phil *)arg;
+    while (!philos->philoss->is_died)
+    {
+        if (get_time() - philos->philoss->last_meal > philos->philoss->t_die)
+        {
+            philo_msg(philos, "died");
+            philos->philoss->is_died = 1;
+            break;
+        }
+    }
     return (NULL);
 }
 
 void    ft_philo(t_philo *philo)
 {
     philo_lanch(philo);
-    // pthread_create(&thread, NULL, philo_routine, philo);
-    // pthread_join(thread, NULL);   
+    int i = 0;
+    t_phil  *philos = malloc(sizeof(t_phil) * philo->philo_nb);
+    while (i < philo->philo_nb)
+    {
+        philos[i].id = i;
+        philos[i].fake_id = i + 1;
+        philos[i].info = philo->info;
+        philos[i].philoss = philo;
+        philos[i].nb_philo = philo->philo_nb;
+        pthread_create(&philo->info->threads[i], NULL, &philoso, (void *)&philos[i]);
+        pthread_create(&philo->info->threads[i], NULL, &check_death, (void *)&philos[i]);
+        i++;
+    }
+    i = 0;
+    while (i < philo->philo_nb)
+    {
+        pthread_join(philo->info->threads[i], NULL);
+        i++;
+    }
 }
 int main(int ac, char **av)
 {
@@ -128,7 +239,6 @@ int main(int ac, char **av)
         return (1);
     if (!is_valid_input(ac, av, philo))
         return (1);
-    printf("philo_nb: %d\nt_die: %d\nt_eat: %d\nt_sleep: %d\nnb_of_meals: %d\n", philo->philo_nb, philo->t_die, philo->t_eat, philo->t_sleep, philo->nb_of_meals);
     ft_philo(philo);
     return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: mjuicha <mjuicha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 17:03:06 by mjuicha           #+#    #+#             */
-/*   Updated: 2024/11/08 19:23:34 by mjuicha          ###   ########.fr       */
+/*   Updated: 2024/11/09 20:44:19 by mjuicha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <sys/time.h>
 int     ft_atoi(const char *str);
 
 typedef struct  s_info
@@ -26,12 +27,35 @@ typedef struct  s_info
     pthread_t           *threads;
 }            t_info;
 
+typedef struct s_phil t_phil;
+
+
 typedef struct  s_philo
 {
     t_info          *info;
+    t_phil         *philos;
+    int             right;
+    int             left;
     int             philo_nb;
+    int             is_time;
+    int             is_died;
+    long            start_time;
+    long            last_meal;
+    pthread_mutex_t mutex;
     int             t_die;
     int             t_eat;
     int             t_sleep;
     int             nb_of_meals;
 }               t_philo;
+
+typedef struct  s_phil
+{
+    int             id;
+    int             fake_id;
+    int             right;
+    int             left;
+    int             nb_philo;
+    t_philo         *philoss;
+    t_info          *info;
+}            t_phil;
+
